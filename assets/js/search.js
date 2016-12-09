c96d26fd9735139750bf8d3edc64ee55ca412472
Fraser Thompson <f.thompson@adinstruments.com>
2016-12-09 15:31:02 +1300

    Author names are now clickable

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,8 +7,12 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+        appendString += '<li><a href="' + item.url + '"><h2>' + item.title + '</h2></a><h5>' + item.date + '</h5>';
+        if (item.content) {
+          appendString += '<p>' + item.content.substring(0, 250) + '...</p></li>';
+        } else {
+          appendString += "<p>No content.</p></li>";
+        }
       }
 
       searchResults.innerHTML = appendString;
